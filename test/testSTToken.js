508ef4323e9074be39181445ec8520c330bca69b
Dav Vendator <eruditoguerrero@gmail.com>
2021-03-24 17:46:30 +0530

    Testing, testing and testing

--- test/testSTToken.js
+++ test/testSTToken.js
@@ -8,12 +8,16 @@ const assert = chai.assert;
 
 let STToken, token, addresses;
 
-describe('STToken', () => {
+const extractReason = (error) =>{
+    let msg = error.message
+    return msg.slice(msg.search("revert")+7, msg.length)
+}
+
+describe('STToken_Owner_Only', () => {
 
     before(async function (){
         STToken = await ethers.getContractFactory("STToken");
         addresses = await ethers.getSigners();
-        console.log(addresses[0].address);
         token = await STToken.deploy();
     })
 
@@ -21,6 +25,14 @@ describe('STToken', () => {
         let [owner] = addresses;
         let ownerBalance = await token.balanceOf(owner.getAddress());
         except(token.totalSupply()).to.eventually.equal(ownerBalance);
+    })
+})
+
+describe("STToken_Transactions", () =>{
+    before(async function (){
+        STToken = await ethers.getContractFactory("STToken");
+        addresses = await ethers.getSigners();
+        token = await STToken.deploy();
     })
 
     it('Should Transfer', async () => {
@@ -36,20 +48,44 @@ describe('STToken', () => {
         let initialBalance = await token.balanceOf(spender.getAddress())
         if (initialBalance > 0) 
             await token.transferFrom(spender.getAddress(), owner.getAddress(), initialBalance);
-        except(token.connect(spender).transfer(userTwo.getAddress(), 10)).to.eventually.rejectedWith("Insufficient Balance") 
+        try{
+            await token.connect(spender).transfer(userTwo.getAddress(), 10);
+        }catch (error){
+           except(extractReason(error)).to.equal("Insufficient Balance");
+        }
+    })
+
+})
+
+describe("STToken_Events", () => {
+    before(async function (){
+        STToken = await ethers.getContractFactory("STToken");
+        addresses = await ethers.getSigners();
+        token = await STToken.deploy();
     })
 
     it('Should Emit Allowance Changed', async () => {
-
+        let [owner, spender] = addresses;
+        await except(
+            await token.increaseApproval(spender.getAddress(), 10)
+         ).to.emit(token, "Approval")
+          .withArgs(await owner.getAddress(), await spender.getAddress(), 10);
     })
 
     it('Should Emit Locked Changed', async () => {
-
+        let [_,user] = addresses;
+        await token.transfer(user.getAddress(), 15);
+        await except(
+            await token.increaseLockedAmount(user.getAddress(), 10)
+        ).to.emit(token, "Locked")
+        .withArgs(await user.getAddress(), 10);
     })
-    
+
     it('Should Emit Unlocked Changed', async () => {
-
+        let [_,user] = addresses;
+        await except(
+            await token.decreaseLockedAmount(user.getAddress(), 5)
+        ).to.emit(token, "Locked")
+        .withArgs(await user.getAddress(), 5);
     })
-
-
 })
