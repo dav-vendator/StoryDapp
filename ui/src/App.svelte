c6529e9b2a976573207604e1c6c08765a1cf370e
Dav Vendator <eruditoguerrero@gmail.com>
2021-05-09 19:04:51 +0530

    UI Partially Completely

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -2,8 +2,7 @@
 	import Account from "./Account.svelte";
 	import Timeline from "./Timeline.svelte";
 	import Event from "./Event.svelte";
-	let isAvailable, wallet;
-	isAvailable = (window.ethereum !== undefined);
+	let isAvailable = (window.ethereum !== undefined);
 </script>
 
 <style>
@@ -12,28 +11,16 @@
 
 <div class="grid-container">
 	<div class="header container">
-			<h1 class="display-4 app-title">StoryDAO</h1>
-			<small class="lead">A decenteralized collaborative story writing app.</small>
+		<h1 class="display-4 app-title">StoryDAO</h1>
+		<small class="lead">A decenteralized collaborative story writing app.</small>
 	</div>
 	<div class="information container">
 		<Account isAvailable/>
 	</div>
 	<div class="content container">
 		<Timeline/>
-		<!--Content-->
 	</div>
 	<div class="events container">
 		<Event/>
 	</div>
 </div>
-<!--Structure-->
-<!-- <header>
-	<div class="container-fluid mw-100">
-		<div class="jumbotron bg-dark text-white text-center fixed">
-			<h1 class="display-4 app-title">StoryDAO</h1>
-			<small class="lead">A decenteralized collaborative story writing app.</small>
-		</div>
-	</div>
-	<div class="overlay-protect"></div>
-	<Account isAvailable/>
-</header> -->
