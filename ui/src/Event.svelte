e23424b6ca20605c61c22174ec6ea779fd07b461
Dav Vendator <eruditoguerrero@gmail.com>
2021-05-10 11:36:07 +0530

    Added event listener.

--- ui/src/Event.svelte
+++ ui/src/Event.svelte
@@ -1,9 +1,12 @@
 <script>
+import { id } from "@ethersproject/hash";
+
     import {ethers} from "ethers";
     import {onMount} from "svelte";
     import {STToken, StoryDAO} from "./abis.js";
     import {tokenAddress, storyAddress} from  "./contracts.js";
     import GreenListItem from "./GreenListItem.svelte";
+    import RedListItem from "./RedListItem.svelte";
 
     let provider = new ethers.providers.Web3Provider(window.ethereum);
     let account = window.ethereum.request({method:'eth_requestAccounts'});
@@ -19,6 +22,37 @@
         await ethereum.on('accountsChanged', function(account) {
             window.alert(`Account changed to ${account}. Please reload!`)
         })
+        
+        story.on("Blacklisted", (address, status) => {
+            events.push({type: 1, address: address, status: status})
+
+            let node = document.createElement("LI");                
+            //Create card element blacklisted
+            let listNode = document.createElement("div")
+
+            listNode.classList.add("card")
+            listNode.classList.add("border")
+            listNode.classList.add("border-danger")
+           
+            let divHeader = document.createElement("div")
+            divHeader.classList.add("card-header")
+
+            let header = document.createTextNode(`Blacklisted`)
+            divHeader.appendChild(header)
+
+            let divBody = document.createElement("div")
+            divBody.classList.add("card-body")
+
+            let bodyText = document.createElement("b")
+            bodyText.innerText = address
+
+            divBody.appendChild(bodyText)
+            listNode.appendChild(divHeader)
+            listNode.appendChild(divBody)
+            node.appendChild(listNode); 
+            node.classList.add("pt-2")
+            document.getElementById("event_list").appendChild(node);   
+        })
 
         story.on("Whitelisted", (address, status) => { 
             events.push({type: 0, address: address, status: status})
@@ -30,7 +64,6 @@
             listNode.classList.add("border")
             listNode.classList.add("border-success")
            
-
             let divHeader = document.createElement("div")
             divHeader.classList.add("card-header")
 
@@ -50,6 +83,52 @@
             node.classList.add("pt-2")
             document.getElementById("event_list").appendChild(node);     
         })
+
+        story.on("SubmissionCreated", (index, isImage, submitter) => {
+            events.push({type: 2, address: submitter, isImage: isImage, index: index})
+            let node = document.createElement("LI");                
+            //Create card element submission created
+            let listNode = document.createElement("div")
+
+            listNode.classList.add("card")
+            listNode.classList.add("border")
+            listNode.classList.add("border-success")
+           
+            let divHeader = document.createElement("div")
+            divHeader.classList.add("card-header")
+
+            let header = document.createTextNode(`Submission Added: ${index} by ${submitter}`)
+            divHeader.appendChild(header)
+
+            let divBody = document.createElement("div")
+            divBody.classList.add("card-body")
+
+            let bodyText = document.createElement("b")
+            if (isImage)
+                bodyText.innerText = "Image Added"
+            else
+                bodyText.innerText = "Text Submission Added!"
+
+            divBody.appendChild(bodyText)
+            listNode.appendChild(divHeader)
+            listNode.appendChild(divBody)
+            node.appendChild(listNode); 
+            node.classList.add("pt-2")
+            document.getElementById("event_list").appendChild(node); 
+        })
+
+        story.on("ProposalAdded" , (id, typeFlag, description, submitter) => {
+
+        })
+
+        story.on("PropsalExecuted", (id) => {
+
+        })
+
+        story.on("Voted", (voter, vote, power, reason) => {
+
+        })
+
     })
 </script>
 
@@ -61,9 +140,24 @@
             {#if item.type == 0}    
                 <li class="pt-2">
                     <GreenListItem header="Whitelisted"
-                        type=true data={`${item.address} whitelisted`}
-                        recipient={storyAddress} sender={item.address} amount="0.01" amountType="ETH"/>
+                        type=true data={item.address}
+                        />
                 </li>
+            {:else if item.type == 1}
+                <RedListItem header="Blacklisted"
+                    data={item.address} />
+            {:else if item.type == 2}
+                <GreenListItem header = {`Submission Added : ${item.index}`}
+                    type=true data = {
+                        () => {
+                            if (item.isImage){
+                                return  "Image Added"
+                            }else{
+                                return "Text Submission Added"
+                            }
+                        }
+                    }
+                />
             {/if}
         {/each}
     </ul>
---
+++ ui/src/RedListItem.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let header;
+    export let data; //data
+    export let sender;
+    export let recipient;
+</script>
+
+<div class="card border border-danger mx-auto">
+    <div class="card-header">{header}</div>
+    <div class="card-body">
+        <p class="card-text">{@html data}</p>
+    </div>
+    {#if sender !== undefined && recipient !==undefined}
+        <div class="card-footer border-danger">
+            <b>From:</b> {sender}
+            <small>To</small>
+            <b>Recipient</b> {recipient}
+        </div>
+    {/if}
+</div>
